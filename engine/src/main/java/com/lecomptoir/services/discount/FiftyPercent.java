89f4bc357f6abc35c52ce4201e0084290cade67a
florian-d-agostino <florian.d-agostino@laplateforme.io>
2026-09-21 16:46:52 +0200

    feat(v2): Remove 10% if above 50€

---
+++ engine/src/main/java/com/lecomptoir/services/discount/FiftyPercent.java
@@ -0,0 +1,25 @@
+package com.lecomptoir.services.discount;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import com.lecomptoir.services.Cart;
+
+public class FiftyPercent {
+    private static final BigDecimal FIFTY = new BigDecimal("50.00");
+    private static final BigDecimal RATE = new BigDecimal("0.10");
+
+
+    public BigDecimal calculate(Cart cart) {
+        BigDecimal total = cart.getTotal();
+
+        if (total.compareTo(FIFTY) > 0) {
+            return total.multiply(RATE).setScale(2, RoundingMode.HALF_UP);
+        }
+        return BigDecimal.ZERO;
+    }
+    
+    
+    
+
+}
